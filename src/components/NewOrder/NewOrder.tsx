41c9b926aede80012a776b33e21631001d244f83
Oleksii Shalyn <pipichindreks.xxx@gmail.com>
2020-01-17 22:44:51 +0200

    feat(Create Order Component): Create order table

--- src/components/NewOrder/NewOrder.tsx
+++ src/components/NewOrder/NewOrder.tsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
-import { INewOrderState } from '../../types/order';
-import { AddProduct } from '../OrderForm/AddProduct/AddProduct';
+import React, { useState } from "react";
+import { INewOrderState } from "../../types/order";
+import { AddProduct } from "../OrderForm/AddProduct/AddProduct";
+import { OrderItemsTable } from "../tables/CreateOrderTable/OrderItemsTable/OrderItemsTable";
 
-const initOrder: INewOrderState = {
+let initOrder: INewOrderState = {
   orderItems: [],
-  totalPrice: 0,
-}
+  totalPrice: 0
+};
+
 
 export const NewOrder = () => {
   const [order, setOrder] = useState(initOrder);
 
   return (
-    <div style={{ padding: '30px 0' }}>
-      <AddProduct order={order} setOrder={setOrder} />
+    <div>
+      <div style={{ padding: "30px 0" }}>
+        <AddProduct order={order} setOrder={setOrder} />
+      </div>
+      {order.orderItems.length > 0 ? <OrderItemsTable order={order} setOrder={setOrder}/> : ''}
+    
     </div>
-  )
-}
+  );
+};
